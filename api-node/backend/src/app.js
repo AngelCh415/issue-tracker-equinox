c10362c9460d7c6fa6221d20533fb044e5bcb322
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-11-24 13:28:31 -0600

    feat: Modify basic enter endpoints

--- api-node/backend/src/app.js
+++ api-node/backend/src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import issueRoutes from './routes/issue.routes.js';
+import projectRoutes from './routes/project.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,16 +17,12 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok', service: 'issue-tracker-api' });
 });
 
-// Example route structure
-// Import your routes here
-// import userRoutes from './routes/user.routes.js';
-// import projectRoutes from './routes/project.routes.js';
-// import issueRoutes from './routes/issue.routes.js';
 
 // Use routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/projects', projectRoutes);
-// app.use('/api/issues', issueRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/issues', issueRoutes);
+
 
 // 404 handler
 app.use((req, res) => {
