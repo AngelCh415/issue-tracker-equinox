7e6c07fc7db5ba5198a4b4e1109bd02d77ee73a9
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-11-24 14:46:24 -0600

    feat: Update endpoints

--- api-node/backend/src/routes/project.routes.js
+++ api-node/backend/src/routes/project.routes.js
@@ -9,14 +9,22 @@ router.get("/", (req, res) => {
     res.json(projects);
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
+    const { name, description } = req.body;
+  
+    if (!name) {
+      return res.status(400).json({ message: 'Project name is required' });
+    }
+  
     const newProject = {
-        id: projects.length + 1,
-        name,
-        description
+      id: projects.length + 1,
+      name,
+      description: description || ''
     };
+  
     projects.push(newProject);
+  
     res.status(201).json(newProject);
-});
+  });
 
 export default router;
