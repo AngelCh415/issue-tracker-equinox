8970a1f6255b1e816dce05bcef1e263cdb7c4cbc
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-11-25 09:00:44 -0600

    feat: Protect routes

--- api-node/frontend/src/App.jsx
+++ api-node/frontend/src/App.jsx
@@ -2,16 +2,48 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Projects from "./pages/Projects";
 import Issues from "./pages/Issues";
+import Navbar from "./components/Navbar";
+
+function PrivateRoute ({ children }) {
+  const user = localStorage.getItem("user");
+  if(!user){
+    return <Navigate to="/" replace/>;
+  }
+  return children;
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/issues" element={<Issues />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="app-shell">
+      <BrowserRouter>
+        {/* Navbar sólo si hay usuario */}
+        {localStorage.getItem("user") && <Navbar />}
+
+        <main className="main-container">
+          <div className="card">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/projects"
+                element={
+                  <PrivateRoute>
+                    <Projects />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/issues"
+                element={
+                  <PrivateRoute>
+                    <Issues />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </main>
+      </BrowserRouter>
+    </div>
   );
 }
 
